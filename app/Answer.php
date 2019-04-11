9f8b121583d801636050c44b71b2b683fc57d057
DesyKartika <desykartika1115@gmail.com>
2019-04-11 12:56:50 +0700

    implement delete functionality for answer

--- app/Answer.php
+++ app/Answer.php
@@ -31,10 +31,12 @@ public static function boot()
  {
 parent::boot();
 
-static::created(function ($answer) {
-  $answer->question->increment('answers_count');
-  $answer->question->save();
-});
-}
+  static::created(function ($answer) {
+    $answer->question->increment('answers_count');
+  });
 
+  static::deleted(function ($answer) {
+    $answer->question->decrement('answers_count');
+  });
+}
 }
